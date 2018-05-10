b0651f29bd6e52ed3d699b2043b43e4fde3dca03
Kent Quirk <kentquirk@gmail.com>
2018-05-10 13:57:31 -0400

    Add command line parsing and an output format

--- cmd/chasm/main.go
+++ cmd/chasm/main.go
@@ -2,17 +2,42 @@ package main
 
 import (
 	"bytes"
-	"encoding/hex"
-	"fmt"
+	"encoding/json"
 	"io"
 	"log"
 	"os"
+
+	arg "github.com/alexflint/go-arg"
 )
 
+type ChasmBinary struct {
+	Name    string `json:"name"`
+	Comment string `json:"comment"`
+	Context string `json:"context"`
+	Data    []byte `json:"data"`
+}
+
+var Contexts = map[byte]string{
+	CtxTest:        "TEST",
+	CtxNodePayout:  "NODE_PAYOUT",
+	CtxEaiTiming:   "EAI_TIMING",
+	CtxNodeQuality: "NODE_QUALITY",
+	CtxMarketPrice: "MARKET_PRICE",
+}
+
 func main() {
+	var args struct {
+		Input   string `arg:"positional"`
+		Output  string `arg:"-o" help:"Output filename"`
+		Comment string `arg:"-c" help:"Comment to embed in the output file."`
+	}
+	arg.MustParse(&args)
+
+	name := "stdin"
 	in := os.Stdin
-	if len(os.Args) > 1 {
-		f, err := os.Open(os.Args[1])
+	if args.Input != "" {
+		name := args.Input
+		f, err := os.Open(name)
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -27,6 +52,25 @@ func main() {
 	if err != nil {
 		log.Fatal(describeErrors(err, buf.String()))
 	}
+
+	out := os.Stdout
+	if args.Output != "" {
+		f, err := os.Create(args.Output)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer f.Close()
+		out = f
+	}
+
 	b := sn.(Script).bytes()
-	fmt.Println(hex.Dump(b))
+	output := ChasmBinary{
+		Name:    name,
+		Comment: args.Comment,
+		Context: Contexts[b[0]],
+		Data:    b,
+	}
+	enc := json.NewEncoder(out)
+	enc.SetIndent("", "  ")
+	enc.Encode(output)
 }
