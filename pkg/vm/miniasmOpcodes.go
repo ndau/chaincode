f265c85ac960ba1214eed70879139b117ec054c6
Kent Quirk <kentquirk@gmail.com>
2019-10-08 10:06:04 -0400

    Add copyright info and license

--- pkg/vm/miniasmOpcodes.go
+++ pkg/vm/miniasmOpcodes.go
@@ -2,6 +2,15 @@
 
 package vm
 
+// ----- ---- --- -- -
+// Copyright 2019 Oneiro NA, Inc. All Rights Reserved.
+//
+// Licensed under the Apache License 2.0 (the "License").  You may not use
+// this file except in compliance with the License.  You can obtain a copy
+// in the file LICENSE in the source distribution or at
+// https://www.apache.org/licenses/LICENSE-2.0.txt
+// - -- --- ---- -----
+
 // these are the opcodes supported by mini-asm
 var opcodeMap = map[string]Opcode{
 	"nop":     OpNop,
