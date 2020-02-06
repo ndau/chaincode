0de0556293d61c85d2c18eb101127ec089cbe193
Ed McNierney <ed@mcnierney.com>
2020-02-06 13:09:44 -0500

    Correct/add copyright messages

--- pkg/vm/opcodes.go
+++ pkg/vm/opcodes.go
@@ -2,6 +2,15 @@
 
 package vm
 
+// ----- ---- --- -- -
+// Copyright 2020 The Axiom Foundation. All Rights Reserved.
+//
+// Licensed under the Apache License 2.0 (the "License").  You may not use
+// this file except in compliance with the License.  You can obtain a copy
+// in the file LICENSE in the source distribution or at
+// https://www.apache.org/licenses/LICENSE-2.0.txt
+// - -- --- ---- -----
+
 // Opcode is a byte used to identify an opcode; we rely on it being a byte in some cases.
 type Opcode byte
 
